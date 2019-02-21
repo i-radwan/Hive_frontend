ee06394dbad9bb8e0119be3c4b7e9c3b2b665794
Ibrahim Radwan <i.radwan1996@gmail.com>
2019-02-21 11:35:43 +0200

    Shouts from gfx

--- js/gfx/gfx.js
+++ js/gfx/gfx.js
@@ -2,7 +2,7 @@ require('two.js');
 let $ = require('jquery');
 let ko = require('knockout');
 
-let gfx = function () {
+let gfx = function (shouter) {
     let mapRow = $('.map-row');
 
     let two = new Two({
@@ -16,6 +16,12 @@ let gfx = function () {
     two.bind('update', function () {
         rect.rotation += 0.01;
     });
+
+    // Ex:
+    // In case of click
+    // shouter.notifySubscribers({row: 0, col: 0}, SHOUT_GRID_CLICK);
+    // In case of drag
+    // shouter.notifySubscribers({row_src: 0, col_src: 0, row_dst: 0, col_dst: 0}, SHOUT_GRID_DRAG);
 };
 
 module.exports = gfx;
