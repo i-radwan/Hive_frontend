1e6fa06b5ccaf612f97a62fabc4024e22067ffda
Ibrahim Radwan <i.radwan1996@gmail.com>
2019-03-03 07:22:25 +0200

    Fix binding

--- js/view_models/control_console.js
+++ js/view_models/control_console.js
@@ -14,7 +14,6 @@ let controlConsoleViewModel = function (runningMode, shouter, map) {
             runningMode(RUNNING_MODE.DESIGN);
             self.playing(false);
         } else {
-            // TODO: Check if all info is good
             runningMode(RUNNING_MODE.SIMULATE);
             self.playing(true);
         }
@@ -39,6 +38,7 @@ let controlConsoleViewModel = function (runningMode, shouter, map) {
         }
 
         runningMode(RUNNING_MODE.DEPLOY);
+        self.playing(true);
     };
 
     shouter.subscribe(function (msg) {
