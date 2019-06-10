51f5656d2fe57242094aa8d228fa4abbbd56f3d2
Ibrahim Radwan <i.radwan1996@gmail.com>
2019-06-10 10:21:34 +0200

    Brace yourselves before the refactoring 😅 Add initial functions to the map model.

--- js/models/map.js
+++ js/models/map.js
@@ -6,14 +6,8 @@ let Map = function () {
     self.height = MAP_INIT_HEIGHT;
     self.width = MAP_INIT_WIDTH;
     self.grid = [];
+    self.objects = {}; // [id, type] ==> [row, col]
 
-    /**
-     * Changes map size.
-     *
-     * @param height    The new map height.
-     * @param width     The new map width.
-     * @param copyOld   Take the top-left corner of the original map.
-     */
     self.changeMapSize = function (height, width, copyOld) {
         console.log("Updating the map h: " + height + " * w:" + width);
 
@@ -24,8 +18,7 @@ let Map = function () {
 
             for (let j = 0; j < width; j++) {
                 newGrid[i][j] = {
-                    robot: undefined,
-                    facility: undefined
+                    objects: []
                 };
             }
         }
@@ -43,11 +36,6 @@ let Map = function () {
         self.height = height;
     };
 
-    /**
-     * Sets the new map grid.
-     *
-     * @param newMap    The new map grid.
-     */
     self.setMap = function (newMap) {
         console.log("Setting the map!");
 
@@ -63,6 +51,146 @@ let Map = function () {
                 self.grid[i][j] = Object.assign({}, newMap[i][j]);
             }
         }
+    };
+
+    self.isFree = function(r, c) {
+        return self.grid[r][c].objects.length === 0;
+    };
+
+    self.isRobotFree = function(r, c) {
+        let objs = self.grid[r][c].objects;
+        let objsLen = objs.length;
+
+        for (let i = 0; i < objsLen; ++i) {
+            if (objs[i].type === MAP_CELL.ROBOT)
+                return false;
+        }
+
+        return true;
+    };
+
+    self.addObject = function(r, c, obj) {
+        self.grid[r][c].objects.push(obj);
+
+        self.objects[[obj.id, obj.type]] = [r, c];
+    };
+
+    self.updateObject = function(r, c, obj) {
+        let objs = self.grid[r][c].objects;
+        let objsLen = objs.length;
+
+        for (let i = 0; i < objsLen; ++i) {
+            if (objs[i].type === obj.type && objs[i].id === obj.id) {
+                objs[i] = obj;
+            }
+        }
+
+        throw "The object to be updated was not found " + obj;
+    };
+
+    self.deleteObject = function(r, c, obj) {
+        let objs = self.grid[r][c].objects;
+        let objsLen = objs.length;
+
+        for (let i = 0; i < objsLen; ++i) {
+            if (objs[i].type === obj.type && objs[i].id === obj.id) {
+                objs.splice(i, 1);
+
+                delete self.objects[[obj.id, obj.type]];
+            }
+        }
+
+        throw "The object to be deleted was not found " + obj;
+    };
+
+    self.getObject = function(r, c, type) {
+        let objs = self.grid[r][c].objects;
+        let objsLen = objs.length;
+
+        for (let i = 0; i < objsLen; ++i) {
+            if (objs[i].type === type) {
+                return objs[i];
+            }
+        }
+
+        return null;
+    };
+
+    self.moveObject = function(r, c, nr, nc, obj) {
+        self.deleteObject(r, c, obj);
+        self.addObject(nr, nc, obj);
+    };
+
+    self.getObjectPos = function(id, type) {
+        return self.objects[[id, type]];
+    };
+
+    self.getRobot = function(r, c) {
+        return self.getObject(r, c, MAP_CELL.ROBOT);
+    };
+
+    self.getInvalidIPRobot = function() {
+        for (const [key, value] of Object.entries(self.objects)) {
+            if (key[1] !== MAP_CELL.ROBOT)
+                continue;
+
+            let rob = self.getRobot(value[0], value[1]);
+
+            if (!rob.ip.match(REG_IP)) {
+                return [value[0], value[1]];
+            }
+        }
+
+        return null;
+    };
+
+    self.isFacilityFree = function(r, c) {
+        let objs = self.grid[r][c].objects;
+        let objsLen = objs.length;
+
+        for (let i = 0; i < objsLen; ++i) {
+            if (objs[i].type === MAP_CELL.RACK || objs[i].type === MAP_CELL.OBSTACLE ||
+                objs[i].type === MAP_CELL.STATION || objs[i].type === MAP_CELL.GATE)
+                return false;
+        }
+
+        return true;
+    };
+
+    self.getFacility = function(r, c) {
+        let objs = self.grid[r][c].objects;
+        let objsLen = objs.length;
+
+        for (let i = 0; i < objsLen; ++i) {
+            if (objs[i].type === MAP_CELL.RACK || objs[i].type === MAP_CELL.OBSTACLE ||
+                objs[i].type === MAP_CELL.STATION || objs[i].type === MAP_CELL.GATE)
+                return objs[i];
+        }
+
+        return null;
+    };
+
+    self.getSpecificFacility = function(r, c, type) {
+        return self.getObject(r, c, type);
+    };
+
+    self.getItemRacks = function(id) {
+        let racks = "";
+
+        for (const [key, value] of Object.entries(self.objects)) {
+            if (key[1] !== MAP_CELL.RACK)
+                continue;
+
+            let rack = self.getSpecificFacility(value[0], value[1], key[1]);
+
+            for (let k = 0; k < rack.items.length; ++k) {
+                if (rack.items[k].id === id) {
+                    racks += rack.id + ", ";
+                }
+            }
+        }
+
+        return racks;
     };
 
     self.changeMapSize(self.height, self.width, false);
