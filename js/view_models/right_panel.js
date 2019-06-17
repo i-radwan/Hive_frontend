fec54d0423de9d2e477dd42bdce1fcdc490715b3
Ibrahim Radwan <i.radwan1996@gmail.com>
2019-06-17 19:59:27 +0200

    Add scrolling arrows.

--- js/view_models/right_panel.js
+++ js/view_models/right_panel.js
@@ -11,55 +11,67 @@ let rightPanelViewModel = function (runningMode, shouter, state, gfxEventHandler
     self.logs = ko.observableArray();
     self.stats = ko.observableArray();
 
+    self.activateScrollUp = ko.observable(false);
+    self.activateScrollDown = ko.observable(false);
+
+    let logsList = $(".rpanel #logs");
+
+    logsList.scroll(function () {
+        self.activateScrollUp(logsList.scrollTop() > 0);
+
+        self.activateScrollDown(
+            logsList.scrollTop() + logsList.innerHeight() < logsList[0].scrollHeight);
+    });
+
     // Sub view models
     self.itemsVM = new itemsPanelViewModel(runningMode, shouter, state, gfxEventHandler);
 
-    // self.logs.push({
-    //     level: LOG_LEVEL.INFO,
-    //     object: LOG_TYPE.ROBOT,
-    //     color: "#17b611",
-    //     msg: "Robot <b>(#3)</b> loaded <b>(6)</b> of item <b>(#2)</b>."
-    // }, {
-    //     level: LOG_LEVEL.WARNING,
-    //     object: LOG_TYPE.ROBOT,
-    //     color: "#007302",
-    //     msg: "Robot <b>(#3)</b> battery is low."
-    // }, {
-    //     level: LOG_LEVEL.ERROR,
-    //     object: LOG_TYPE.ROBOT,
-    //     color: "#007302",
-    //     msg: "Robot <b>(#3)</b> is disconnected."
-    // }, {
-    //     level: LOG_LEVEL.INFO,
-    //     object: LOG_TYPE.ORDER,
-    //     msg: "Order <b>(xyz)</b> is fulfilled."
-    // }, {
-    //     level: LOG_LEVEL.INFO,
-    //     object: LOG_TYPE.TEXT,
-    //     msg: "Simulation Paused"
-    // }, {
-    //     level: LOG_LEVEL.INFO,
-    //     object: LOG_TYPE.TEXT,
-    //     msg: "Simulation Resumed"
-    // }, {
-    //     level: LOG_LEVEL.INFO,
-    //     object: LOG_TYPE.ORDER,
-    //     msg: "Order <b>(abc)</b> is fulfilled."
-    // }, {
-    //     level: LOG_LEVEL.INFO,
-    //     object: LOG_TYPE.ROBOT,
-    //     color: "#626678",
-    //     msg: "Robot <b>(#6)</b> loaded <b>(4)</b> of item <b>(#1)</b>."
-    // }, {
-    //     level: LOG_LEVEL.WARNING,
-    //     object: LOG_TYPE.ROBOT,
-    //     color: "#731121",
-    //     msg: "Robot <b>(#2)</b> battery is low."
-    // }, {
-    //     level: LOG_LEVEL.ERROR,
-    //     object: LOG_TYPE.RACK,
-    //     msg: "Rack <b>(#4)</b> is empty."
-    // });
+    self.logs.push({
+        level: LOG_LEVEL.INFO,
+        object: LOG_TYPE.ROBOT,
+        color: "#17b611",
+        msg: "Robot <b>(#3)</b> loaded <b>(6)</b> of item <b>(#2)</b>."
+    }, {
+        level: LOG_LEVEL.WARNING,
+        object: LOG_TYPE.ROBOT,
+        color: "#007302",
+        msg: "Robot <b>(#3)</b> battery is low."
+    }, {
+        level: LOG_LEVEL.ERROR,
+        object: LOG_TYPE.ROBOT,
+        color: "#007302",
+        msg: "Robot <b>(#3)</b> is disconnected."
+    }, {
+        level: LOG_LEVEL.INFO,
+        object: LOG_TYPE.ORDER,
+        msg: "Order <b>(xyz)</b> is fulfilled."
+    }, {
+        level: LOG_LEVEL.INFO,
+        object: LOG_TYPE.TEXT,
+        msg: "Simulation Paused"
+    }, {
+        level: LOG_LEVEL.INFO,
+        object: LOG_TYPE.TEXT,
+        msg: "Simulation Resumed"
+    }, {
+        level: LOG_LEVEL.INFO,
+        object: LOG_TYPE.ORDER,
+        msg: "Order <b>(abc)</b> is fulfilled."
+    }, {
+        level: LOG_LEVEL.INFO,
+        object: LOG_TYPE.ROBOT,
+        color: "#626678",
+        msg: "Robot <b>(#6)</b> loaded <b>(4)</b> of item <b>(#1)</b>."
+    }, {
+        level: LOG_LEVEL.WARNING,
+        object: LOG_TYPE.ROBOT,
+        color: "#731121",
+        msg: "Robot <b>(#2)</b> battery is low."
+    }, {
+        level: LOG_LEVEL.ERROR,
+        object: LOG_TYPE.RACK,
+        msg: "Rack <b>(#4)</b> is empty."
+    });
 
     self.stats.push({
         key: "Throughput",
@@ -161,17 +173,15 @@ let rightPanelViewModel = function (runningMode, shouter, state, gfxEventHandler
     });
 
     self.addLog = function (log) {
-        let container = $(".rpanel #logs");
-
         let userScrollingUp =
-            container.scrollTop() + container.innerHeight() < container[0].scrollHeight;
+            logsList.scrollTop() + logsList.innerHeight() < logsList[0].scrollHeight;
 
         // Add the log
         self.logs.push(log);
 
         // Scroll view to bottom if the user is already not scrolling to top
         if (!userScrollingUp)
-            container.animate({scrollTop: container[0].scrollHeight}, 250);
+            logsList.animate({scrollTop: logsList[0].scrollHeight}, 250);
     };
 
     self.updateStats = function (key, value) {
@@ -204,14 +214,21 @@ let rightPanelViewModel = function (runningMode, shouter, state, gfxEventHandler
             self.activePanel(RIGHT_PANEL.ITEMS);
             self.logs.removeAll();
             self.stats.removeAll();
-        }
-        else {
+        } else {
             self.activePanel(RIGHT_PANEL.LOGS);
         }
     });
 
     self.handleEsc = function () {
 
+    };
+
+    self.scrollUp = function () {
+        logsList.animate({scrollTop: 0}, 250);
+    };
+
+    self.scrollDown = function () {
+        logsList.animate({scrollTop: logsList[0].scrollHeight}, 250);
     };
 };
 
